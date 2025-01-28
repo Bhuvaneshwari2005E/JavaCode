366933bec55864cfba81ca9001d167c882fe7f1c
Bhuvaneshwari2005E <bhuvana.lak05@gmail.com>
2025-01-28 07:32:16 +0530

    Add files via upload

---
+++ GCD.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class GCD {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+System.out.print("Enter a: ");
+int a = sc.nextInt();
+System.out.print("Enter b: ");
+int b = sc.nextInt();
+while (b != 0) {
+    int remainder = a % b; 
+    a = b;                 
+    b = remainder;         
+}
+System.out.print("GCD: " + a);
+}}
